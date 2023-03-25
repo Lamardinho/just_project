8b92e1332d57d6fe93a3043cf7b7bff455e59d74
Ilya Slezkin <ilia.slezkin@r1.team>
2023-03-25 17:49:24 +0500

    mail_service: реализация почтового сервиса

---
+++ src/main/java/com/example/just_project/project_mail/EmailServiceApp.java
@@ -0,0 +1,30 @@
+package com.example.just_project.project_mail;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+class EmailServiceApp {
+
+    @NonNull
+    private final JavaMailSender emailSender;
+
+    @Value("${my-app.mail-server.username}")
+    private String appMailUserName;
+
+    public void sendMessage(@NonNull final SendingMailDto dto) {
+        val message = new SimpleMailMessage();
+        message.setFrom(appMailUserName);
+        message.setTo(dto.getAddress());
+        message.setSubject(dto.getSubject());
+        message.setText(dto.getText());
+
+        emailSender.send(message);
+    }
+}
