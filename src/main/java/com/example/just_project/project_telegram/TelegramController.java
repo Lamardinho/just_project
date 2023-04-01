05e17953efe8521709418b8e691262bd9cebb911
Ilya Slezkin <ilia.slezkin@r1.team>
2023-04-02 01:16:30 +0500

    project_telegram: добавление отправки сообщений в телеграм

---
+++ src/main/java/com/example/just_project/project_telegram/TelegramController.java
@@ -0,0 +1,34 @@
+package com.example.just_project.project_telegram;
+
+import com.example.just_project.common.aop.TrackExecutionTime;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@Tag(name = "Сервис telegram сообщений", description = "Сервис telegram сообщений")
+@RequestMapping("/api/telegram")
+@RestController
+@RequiredArgsConstructor
+@Log4j2
+public class TelegramController {
+
+    @NonNull
+    private final TelegramMessageService messageService;
+
+    @PostMapping("/send-message")
+    @TrackExecutionTime
+    public ResponseEntity<String> sendMessage(
+            @RequestBody @Valid final TelegramSendMessageDto dto
+    ) {
+        messageService.sendMessage(dto);
+        return ResponseEntity.ok("Message sent successfully");
+    }
+}
---
+++ src/main/java/com/example/just_project/project_telegram/TelegramMessageService.java
@@ -0,0 +1,28 @@
+package com.example.just_project.project_telegram;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@RequiredArgsConstructor
+@Validated
+class TelegramMessageService {
+
+    @NonNull
+    private final RestTemplate restTemplate;
+
+    @Value("${telegram.bot.token}")
+    private String botToken;
+    @Value("${telegram.bot.send-msg-post-url}")
+    private String sendMsgPostUrl;
+
+    public void sendMessage(@NonNull TelegramSendMessageDto dto) {
+        val url = String.format(sendMsgPostUrl, botToken, dto.getAddress(), dto.getText());
+        restTemplate.postForEntity(url, null, String.class);
+    }
+}
