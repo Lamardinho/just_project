66ee7173108c7328831574689e19e96951b3bedb
Ilya Slezkin <ilia.slezkin@r1.team>
2023-04-09 12:25:00 +0500

    develop: JasperReportsService.java added JasperReportService.kt removed

---
+++ src/main/java/com/example/just_project/common/enums/JasperReportExtension.java
@@ -0,0 +1,11 @@
+package com.example.just_project.common.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum JasperReportExtension {
+    PDF,
+    XLSX
+}
