d73ad3b74feee1cfbed08ee02472072d8ffa9afa
Ilya Slezkin <ilia.slezkin@r1.team>
2023-04-15 00:29:15 +0500

    develop: ForbiddenExceptionHandler added log.error

--- src/main/java/com/example/just_project/common/exceptionhandlers/ForbiddenExceptionHandler.java
+++ src/main/java/com/example/just_project/common/exceptionhandlers/ForbiddenExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.just_project.common.exceptionhandlers;
 
 import com.example.just_project.common.util.ContractResult;
 import lombok.NonNull;
+import lombok.extern.log4j.Log4j2;
 import lombok.val;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -10,12 +11,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.client.HttpClientErrorException;
 
 @ControllerAdvice
+@Log4j2
 public class ForbiddenExceptionHandler {
 
     @ExceptionHandler(value = {HttpClientErrorException.class})
     public ResponseEntity<ContractResult<String>> handleAccessDeniedException(
             @NonNull HttpClientErrorException ex
     ) {
+        log.error(ex.getMessage());
         val httpStatus = ex.getStatusCode();
         val contractResult = new ContractResult<String>();
         contractResult.getViolations().add(ex.getMessage());
